72256e19eea1b99838959c5c074a590b72bfd357
Rohitkumar Arasanipalai <rohitkumar97@gmail.com>
2017-12-18 18:24:19 +0000

    Removed redundant files

--- rnn_noise_removal_2.py
+++ rnn_noise_removal_2.py
@@ -28,13 +28,13 @@ def get_data(filename):
 	pink_noise,fft_pink = ng.pink_noise(np.shape(data)[0])
 	print(Fs)
 
-	bandlimit_white_noise = butter_lowpass(white_noise, 20000,Fs,order = 10)
+	bandlimit_white_noise = butter_lowpass(white_noise, 5000,Fs,order = 10)
 	scaled_bandlimit_white_noise = (np.max(data)/np.max(bandlimit_white_noise))*bandlimit_white_noise
 
-	bandlimit_pink_noise = butter_lowpass(pink_noise, 20000,Fs,order = 10)
+	bandlimit_pink_noise = butter_lowpass(pink_noise, 5000,Fs,order = 10)
 	scaled_bandlimit_pink_noise = (np.max(data)/np.max(bandlimit_pink_noise))*bandlimit_pink_noise
-	data = data[:,0]
-	data_noise = data + np.abs(scaled_bandlimit_pink_noise)
+	data_noise = data + np.abs(scaled_bandlimit_white_noise)
+	#data_noise = data + np.abs(scaled_bandlimit_pink_noise) #Uncomment and comment the earlier line for pink noise
 	return data_noise, data, Fs
 
 def data_preprocessing(trainX, trainY):
@@ -56,13 +56,21 @@ def play_file(data,Fs):
 	time.sleep(ti)
 	sd.stop()
 
+def measure_snr(noisy, data):
+	noise = noisy - data
+	pwr_noise = (np.max(noise)**2)/noise.size
+	pwr_data = (np.max(data)**2)/data.size
+	snr = pwr_data/pwr_noise
+	return snr
+
 def main():
 	#data preprocessing step
-	trainX_o,trainY_o, Fs = get_data('output.wav')
+	trainX_o,trainY_o, Fs = get_data('Mockingbird.wav')
 	print('Playing noisy data')
-	play_file(trainX_o, Fs)
+	print(measure_snr(trainX_o,trainY_o))
+	#play_file(trainX_o, Fs)
 	print('Playing actual data')
-	play_file(trainY_o, Fs)
+	#play_file(trainY_o, Fs)
 	trainX, trainY = data_preprocessing(trainX_o, trainY_o)
 	print(trainX.shape, trainY.shape)
 	#Neural Network Model
@@ -70,7 +78,7 @@ def main():
 	model.add(LSTM(8, input_shape=(tap, 1)))
 	model.add(Dense(tap))
 	model.compile(loss='mean_squared_error', optimizer='adam')
-	hist = model.fit(trainX, trainY, epochs=20, batch_size=1000, verbose=2)
+	hist = model.fit(trainX, trainY, epochs=5, batch_size=1000, verbose=2)
 	loss = list(hist.history['loss'])
 	plt.plot(loss)
 	plt.show()
@@ -81,14 +89,16 @@ def main():
 	predict = temp1 + temp2
 	predict = np.array(predict)
 	print('Output of neural network')
-	play_file(predict,Fs)
+	print(measure_snr(predict,trainY_o))
+	#play_file(predict,Fs)
 	save_file('output_training_data_50.wav',predict,Fs)
 
-	trainX_o,trainY_o, Fs = get_data('output_test.wav')
+	trainX_o,trainY_o, Fs = get_data('Australian.wav')
 	print('Playing noisy data')
-	play_file(trainX_o, Fs)
+	print(measure_snr(trainX_o,trainY_o))
+	#play_file(trainX_o, Fs)
 	print('Playing actual data')
-	play_file(trainY_o, Fs)
+	#play_file(trainY_o, Fs)
 	trainX, trainY = data_preprocessing(trainX_o, trainY_o)
 	yhat = model.predict(trainX, batch_size = 1000, verbose = 0)
 	a_yhat = yhat*(np.amax(trainY_o/np.amax(trainY_o))/np.amax(yhat))*np.amax(trainY_o)
@@ -97,7 +107,8 @@ def main():
 	predict = temp1 + temp2
 	predict = np.array(predict)
 	print('Output of neural network')
-	play_file(predict,Fs)
+	print(measure_snr(predict,trainY_o))
+	#play_file(predict,Fs)
 	save_file('output_testing_data_50.wav',predict, Fs)
 
 
